55db2c04c3a2ca14e6087576ece7111fefe716a9
Jeff <Jafaragha77@gmail.com>
2023-04-08 14:30:33 +0330

    - added viewmodel and repository

--- app/src/main/java/com/example/mealsapp/ui/meals/MealsActivity.kt
+++ app/src/main/java/com/example/mealsapp/ui/meals/MealsActivity.kt
@@ -1,15 +1,12 @@
-package com.example.mealsapp
+package com.example.mealsapp.ui.meals
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.mealsapp.ui.meals.vm.MealsCategoriesViewModel
 import com.example.mealsapp.ui.theme.MealsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,27 +16,21 @@ class MealsActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MealsAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MealsCategoriesScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MealsCategoriesScreen() {
+    val viewModel = hiltViewModel<MealsCategoriesViewModel>()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MealsAppTheme {
-        Greeting("Android")
+        MealsCategoriesScreen()
     }
 }
---
+++ app/src/main/java/com/example/mealsapp/ui/meals/repo/MealsRepository.kt
@@ -0,0 +1,5 @@
+package com.example.mealsapp.ui.meals.repo
+
+import javax.inject.Inject
+
+class MealsRepository @Inject constructor()
