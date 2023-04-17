2520dccf913efb1cdd92d5d02a9ca72cd93ee9ea
Jeff <Jafaragha77@gmail.com>
2023-04-17 11:14:14 +0330

    - implementing expanding list item

--- app/src/main/java/com/example/mealsapp/ui/meals/MealsCategoryListScreen.kt
+++ app/src/main/java/com/example/mealsapp/ui/meals/MealsCategoryListScreen.kt
@@ -1,17 +1,21 @@
 package com.example.mealsapp.ui.meals
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CutCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -39,6 +43,9 @@ fun MealsCategoryListScreen() {
 @Composable
 fun Meal(meal: Meal) {
 
+
+    var isDescriptionExpanded by remember { mutableStateOf(false) }
+
     Card(
         shape = CutCornerShape(8.dp),
         elevation = 2.dp,
@@ -47,27 +54,53 @@ fun Meal(meal: Meal) {
             .padding(16.dp)
     ) {
 
-        Row(Modifier.clickable { }) {
+        Row(
+            Modifier
+                .animateContentSize()
+                .clickable { }) {
             // Image
             AsyncImage(
                 model = meal.imageUrl,
                 contentDescription = "Meal Picture",
                 modifier = Modifier
                     .size(88.dp)
-                    .padding(4.dp),
-                contentScale = ContentScale.Crop
+                    .padding(4.dp)
+                    .align(Alignment.CenterVertically),
+                contentScale = ContentScale.Crop,
             )
 
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
                     .padding(16.dp)
+                    .fillMaxWidth(0.8f)
             ) {
                 Text(
                     text = meal.name,
                     style = MaterialTheme.typography.h6
                 )
+                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                    Text(
+                        text = meal.description,
+                        style = MaterialTheme.typography.subtitle2,
+                        textAlign = TextAlign.Start,
+                        maxLines = if (isDescriptionExpanded) 6 else 3,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
             }
+
+            Icon(
+                imageVector =
+                if (isDescriptionExpanded) Icons.Filled.KeyboardArrowUp
+                else Icons.Filled.ArrowDropDown,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(24.dp)
+                    .align(if (isDescriptionExpanded) Alignment.Bottom else Alignment.CenterVertically)
+                    .clickable { isDescriptionExpanded = !isDescriptionExpanded }
+            )
 
         }
     }
